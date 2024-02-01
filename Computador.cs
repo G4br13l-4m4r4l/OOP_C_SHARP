854d4dfaf1587079e60c8c9bf5d4a008d64a3b70
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2024-02-01 15:31:28 -0300

    first commit

---
+++ Computador.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo
+{
+    internal class Computador
+    {
+        String placa_mae;
+        String Processador;
+        String Armazenamento;
+        String placa_video;
+        int RAM;
+        bool ligada = false;
+
+        public Computador(String placa_mae, String Processador, String Armazenamento, int RAM)
+        {
+            this.placa_mae = placa_mae;
+            this.Processador = Processador;
+            this.Armazenamento = Armazenamento;
+            this.RAM = RAM;
+            try
+            {
+                Console.WriteLine("Existe placa de vídeo? (s/n)");
+                String resp = Console.ReadLine();
+                if(resp == "s")
+                {
+                    Console.WriteLine("qual a sua placa? ");
+                    this.placa_video = Console.ReadLine();
+                }
+                Console.WriteLine("Máquina montada!!!!");
+            }catch (FormatException f)
+            {
+                Console.WriteLine(f.Message);
+            }
+        }
+
+        public void Upgrade() {
+            try
+            {
+                Console.Write("Qual componente deseja realizar upgrade?\n1-Placa mãe\n2-Processador\n3-Armazenamento\n4-RAM\n5-Placa de Vídeo");
+                int componente = Convert.ToInt32(Console.ReadLine());
+                switch (componente)
+                {
+                    case 1:
+                        Console.Write("Digite qual é a placa que deseja colocar: ");
+                        placa_mae = Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.Write("Digite qual é o processador que deseja: ");
+                        Processador = Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.Write("Digite qual é o armazenamento que deseja: ");
+                        Armazenamento = Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.Write("Digite qual é a RAM que deseja: ");
+                        RAM = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 5:
+                        Console.Write("Digite qual é a placa de vídeo que deseja: ");
+                        placa_video = Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("Command not found!");
+                        break;
+                }
+            }catch (ArithmeticException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+            catch (FormatException f)
+            {
+                Console.WriteLine(f.Message);
+            }
+        }
+
+        public void Ligar_desligar()
+        {
+            if (ligada)
+            {
+                Console.WriteLine("Máquina está ligada ===> desligando...");
+                for (int i = 0; i <= 100; i++)
+                {
+                    AtualizarLoading(i);
+                    Thread.Sleep(50);
+                }
+                Console.WriteLine("\nProcesso concluido!\nmáquina desligada!\n");
+                ligada = false;
+            }
+            else
+            {
+                Console.WriteLine("Máquina está desligada ===> ligando...");
+                for (int i = 0; i <= 100; i++)
+                {
+                    AtualizarLoading(i);
+                    Thread.Sleep(50);
+                }
+                ligada = true;
+                Console.WriteLine("\nProcesso concluído!\n Máquina ligada!\n");
+            }
+        }
+
+        static void AtualizarLoading(int percentual)
+        {
+            int tamanhoBarra = Console.WindowWidth - 20; 
+            int quantidadeCaracteres = (int)Math.Ceiling((percentual / 100.0) * tamanhoBarra);
+
+            string barraDeProgresso = new string('=', quantidadeCaracteres);
+            string espacos = new string(' ', tamanhoBarra - quantidadeCaracteres);
+
+            Console.Write($"\r[{barraDeProgresso}{espacos}] {percentual}%");
+        }
+
+        public void utilizar()
+        {
+            Console.WriteLine("Deseja navegar na internet? (s/n)");
+            String resp = Console.ReadLine();
+            if(resp == "s")
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continuar = true;
+            
+            Console.WriteLine("=======================================\n\tW3lc8m3_T8_Cyberworld!\n=======================================\n");
+            Console.WriteLine(" _     _  _______  ___      _______  _______  __   __  _______");
+            Console.WriteLine("| | _ | ||       ||   |    |       ||       ||  |_|  ||       |");
+            Console.WriteLine("| || || ||    ___||   |    |       ||   _   ||       ||    ___|");
+            Console.WriteLine("|       ||   |___ |   |    |       ||  | |  ||       ||   |___");
+            Console.WriteLine("|       ||    ___||   |___ |      _||  |_|  ||       ||    ___|");
+            Console.WriteLine("|   _   ||   |___ |       ||     |_ |       || ||_|| ||   |___");
+            Console.WriteLine("|__| |__||_______||_______||_______||_______||_|   |_||_______|");
+            Console.Beep();
+
+
+            Console.Write("Digite o seu nome: ");
+            String name = Console.ReadLine();
+
+            Console.WriteLine($"Olá {name}");
+            Console.WriteLine("Deseja montar um computador? (s/n)");
+            String resp = Console.ReadLine();
+
+
+            if (resp == "s")
+            {
+                Console.Write("Digite qual a sua placa: ");
+                String placa = Console.ReadLine();
+                Console.Write("Digite qual o seu processador: ");
+                String processador = Console.ReadLine();
+                Console.Write("Digite qual o tipo de armazenamento: ");
+                String armazenamento = Console.ReadLine();
+                Console.Write("Digite a quantidade de RAM: ");
+                int RAM = Convert.ToInt32(Console.ReadLine());
+
+                Computador computer = new Computador(placa, processador, armazenamento, RAM);
+                while (continuar)
+                {
+                    try
+                    {
+                        Console.WriteLine("O que deseja fazer agora? (1-ligar_Desligar/2-utilizar/3-/4-/5-Sair)");
+                        int actions = Convert.ToInt32(Console.ReadLine());
+                        switch (actions)
+                        {
+                            case 1:
+                                computer.Ligar_desligar();
+                                break;
+                            case 2:
+                                computer.utilizar();
+                                break;
+                            case 3:
+                                break;
+                            case 4:
+                                break;
+                            case 5:
+                                continuar = false;
+                                Console.WriteLine("\n=================================\n\tExiting program\n=================================\n");
+                                break;
+                            default:
+                                Console.WriteLine("Command not found!");
+                                break;
+                        }
+                    }
+                    catch (FormatException f)
+                    {
+                        Console.WriteLine(f.ToString());
+                    }
+                }
+            }
+ 
+
+            Console.ReadKey();
+        }
+    }
+}
